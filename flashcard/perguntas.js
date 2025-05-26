6b827641927f81f37655ddc588c072f50d61fa78
matheuscj09 <matheus.ruckel@escola.pr.gov.br>
2025-05-26 19:17:19 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que significa a lógica de programação e por que ela é importante',
+    'Lógica de programação é o raciocínio usado para resolver problemas por meio de códigos. Ela é importante porque permite criar soluções eficientes e funcionais, organizando os passos que o programa deve seguir.'
+)
+
+criaCartao(
+    'Geografia',
+    'Sabe qual é a maior montanha do mundo?',
+    'A maior montanha do mundo é o Monte Everest.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um bug na programação?',
+    'É um erro ou falha no código que faz com que o programa não funcione como deveria.'
+)
